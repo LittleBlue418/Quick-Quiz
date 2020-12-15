d4ceae48b30c252c48525c64c08e71b6eef5fe8c
LittleBlue418 <hollyathomas88@gmail.com>
2020-12-15 16:26:46 +0100

    Conditional rendering of quiz buttons using states

--- src/App.tsx
+++ src/App.tsx
@@ -24,10 +24,20 @@ const App = () => {
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
-
   const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
 
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -41,22 +51,34 @@ const App = () => {
   return (
     <div className="App">
       <h1>React Quiz</h1>
-      <button className="start" onClick={startTrivia}>
-        Start
-      </button>
-      <p className="score">Score: </p>
-      <p>Loading questions ...</p>
-      {/*<QuestionCard
-        questionNr={number +1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      />*/}
-      <button className="next" onClick={nextQuestion}>
+
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+      ) : null }
+
+      {!gameOver ? <p className="score">Score: </p> : null}
+
+      {loading ? <p>Loading questions ...</p> : null }
+
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNr={number +1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+
+      {!gameOver && !loading && userAnswers.length === number +1 && number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
+      ) : null }
+
     </div>
   );
 }
